c550b7a5be786a7b135a69913b026cda12ebecaa
Josh Anderson <joshand@cisco.com>
2023-11-20 20:17:47 -0600

    update sheet index

--- module/item/sheets/index.js
+++ module/item/sheets/index.js
@@ -1,31 +1,30 @@
 /**
- * FVTT-Genesys
- * Unofficial implementation of the Genesys RPG for Foundry
- *
- * @author Mezryss
- * @file Item Sheet Registration
+ * The Edge of Anarchy game system for Foundry Virtual Tabletop
+ * A system for playing EoA
+ * Author: Josh Anderson
+ * Software License: MIT
  */
 
-import AbilitySheet from '@/vue/sheets/item/AbilitySheet.vue';
-import ArmorSheet from '@/item/sheets/ArmorSheet';
-import EquipmentSheet from '@/vue/sheets/item/EquipmentSheet.vue';
-import InjurySheet from '@/vue/sheets/item/InjurySheet.vue';
-import ItemQualitySheet from '@/vue/sheets/item/ItemQualitySheet.vue';
-import SkillSheet from '@/vue/sheets/item/SkillSheet.vue';
-import TalentSheet from '@/vue/sheets/item/TalentSheet.vue';
-import WeaponSheet from '@/item/sheets/WeaponSheet';
-import ArchetypeSheet from '@/item/sheets/ArchetypeSheet';
-import CareerSheet from '@/item/sheets/CareerSheet';
-import VueSheet from '@/vue/VueSheet';
-import GenesysItemSheet from '@/item/GenesysItemSheet';
-import { GenesysItemSheetData, ItemSheetContext } from '@/vue/SheetContext';
+import AbilitySheet from '../../vue/sheets/item/AbilitySheet.vue';
+import ArmorSheet from './ArmorSheet.js';
+import EquipmentSheet from '../../vue/sheets/item/EquipmentSheet.vue';
+import InjurySheet from '../../vue/sheets/item/InjurySheet.vue';
+import ItemQualitySheet from '../../vue/sheets/item/ItemQualitySheet.vue';
+import SkillSheet from '../../vue/sheets/item/SkillSheet.vue';
+import TalentSheet from '../../vue/sheets/item/TalentSheet.vue';
+import WeaponSheet from './WeaponSheet.js';
+import ArchetypeSheet from './ArchetypeSheet.js';
+import CareerSheet from './CareerSheet.js';
+import VueSheet from '../../vue/VueSheet';
+import GameItemSheet from '../GameItemSheet';
+import { GameItemSheetData, ItemSheetContext } from '../../vue/SheetContext';
 
 /**
  * Constructs a vue-based ItemSheet subclass with no extra processing - just the sheet itself.
  * @param sheetClass Vue component to use for the sheet.
  */
 function basicSheet(sheetClass: any) {
-	return class extends VueSheet(GenesysItemSheet) {
+	return class extends VueSheet(GameItemSheetData) {
 		override get vueComponent() {
 			return sheetClass;
 		}
@@ -33,7 +32,7 @@ function basicSheet(sheetClass: any) {
 		override async getVueContext(): Promise<ItemSheetContext | undefined> {
 			return {
 				sheet: this,
-				data: (await this.getData()) as GenesysItemSheetData,
+				data: (await this.getData()) as GameItemSheetData,
 			};
 		}
 	};
@@ -45,52 +44,52 @@ function basicSheet(sheetClass: any) {
 export function register() {
 	Items.unregisterSheet('core', ItemSheet);
 
-	Items.registerSheet('genesys', basicSheet(AbilitySheet), {
+	Items.registerSheet('eoa', basicSheet(AbilitySheet), {
 		types: ['ability'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', ArmorSheet, {
+	Items.registerSheet('eoa', ArmorSheet, {
 		types: ['armor'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', basicSheet(EquipmentSheet), {
+	Items.registerSheet('eoa', basicSheet(EquipmentSheet), {
 		types: ['consumable', 'container', 'gear'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', basicSheet(InjurySheet), {
+	Items.registerSheet('eoa', basicSheet(InjurySheet), {
 		types: ['injury'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', basicSheet(ItemQualitySheet), {
+	Items.registerSheet('eoa', basicSheet(ItemQualitySheet), {
 		types: ['quality'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', basicSheet(SkillSheet), {
+	Items.registerSheet('eoa', basicSheet(SkillSheet), {
 		types: ['skill'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', basicSheet(TalentSheet), {
+	Items.registerSheet('eoa', basicSheet(TalentSheet), {
 		types: ['talent'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', WeaponSheet, {
+	Items.registerSheet('eoa', WeaponSheet, {
 		types: ['weapon'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', ArchetypeSheet, {
+	Items.registerSheet('eoa', ArchetypeSheet, {
 		types: ['archetype'],
 		makeDefault: true,
 	});
 
-	Items.registerSheet('genesys', CareerSheet, {
+	Items.registerSheet('eoa', CareerSheet, {
 		types: ['career'],
 		makeDefault: true,
 	});
